37c8199392781b7296b1d8005db0732392d5440d
sologuboved <sologuboved@gmail.com>
2018-03-16 23:44:00 +0300

    eliminated misleading variable names

--- read_out.py
+++ read_out.py
@@ -1,27 +1,27 @@
-FNAME_OUT = 'true_events.txt'
-FNAME_IN = 'true_events{}.txt'
+SOURCE_FNAME = 'true_events.txt'
+TARGET_FNAME = 'true_events{}.txt'
 
 
-def read_out(fname_out, fname_in, lim=1000):
+def read_out(source_fname, target_fname, lim=1000):
 
     def write_in(indexed_fname):
         ind = 0
-        with open(indexed_fname, 'wb') as in_handler:
+        with open(indexed_fname, 'wb') as target_handler:
             while ind < lim:
                 ind += 1
                 try:
-                    in_handler.write(next(out_handler))
+                    target_handler.write(next(source_handler))
                 except StopIteration:
                     return True
 
     iteration = 0
-    with open(fname_out, 'rb') as out_handler:
+    with open(source_fname, 'rb') as source_handler:
         while True:
             iteration += 1
-            end = write_in(fname_in.format(iteration))
+            end = write_in(target_fname.format(iteration))
             if end:
                 break
 
 
 if __name__ == '__main__':
-    read_out(FNAME_OUT, FNAME_IN)
+    read_out(SOURCE_FNAME, TARGET_FNAME)
