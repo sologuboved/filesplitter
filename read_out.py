33068cd2f4870a4f54e2047f01d101d7e7a760f8
sologuboved <sologuboved@gmail.com>
2018-03-17 00:15:01 +0300

    minor refactoring

--- read_out.py
+++ read_out.py
@@ -9,17 +9,15 @@ def read_out(source_fname, target_fname, lim=1000):
         with open(indexed_fname, 'wb') as target_handler:
             while ind < lim:
                 ind += 1
-                try:
-                    target_handler.write(next(source_handler))
-                except StopIteration:
-                    return True
+                target_handler.write(next(source_handler))
 
     iteration = 0
     with open(source_fname, 'rb') as source_handler:
         while True:
             iteration += 1
-            end = write_in(target_fname.format(iteration))
-            if end:
+            try:
+                write_in(target_fname.format(iteration))
+            except StopIteration:
                 break
 
 
